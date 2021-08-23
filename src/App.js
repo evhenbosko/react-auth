861af999abcbcfbefdadc91518dd04bddc48b022
evhenbosko <evhenbosko@gmail.com>
2021-08-23 21:27:37 +0300

    auth ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import Loader from'./components/Loader'
+import Me from'./components/Me'
+import { BrowserRouter ,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<BrowserRouter>
+  <NavBar/>
+    <main className="form-signin">
+      <Route path='/' exact component={Me}/>
+      <Route path='/login'component={Login}/>
+      <Route path='/SignUp'component={SignUp}/>
+      <Route path='/Loading' component={Loader}/>
+      <Route path='/Loading2' component={Loader2}/>
+    </main>    
+</BrowserRouter>
+
+
+
+
+
     </div>
   );
 }
